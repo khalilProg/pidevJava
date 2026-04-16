3ddbd66aedaf9a5bddade37dab3b36866eb34b72
YChaffai <yassinechaffai4@gmail.com>
2026-04-16 10:27:02 +0100

    Ui fixes

--- src/main/java/tn/esprit/tests/MainFX.java
+++ src/main/java/tn/esprit/tests/MainFX.java
@@ -12,9 +12,17 @@ public class MainFX extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/AdminAfficherCommandes.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root, 1000, 700);
-        primaryStage.setTitle("BLOODLINK — Gestion des Commandes");
+        primaryStage.setTitle("BLOODLINK — BackOffice");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        Stage secondaryStage = new Stage();
+        FXMLLoader loader2 = new FXMLLoader(getClass().getResource("/AfficherCommandes.fxml"));
+        Parent root2 = loader2.load();
+        Scene scene2 = new Scene(root2, 1000, 700);
+        secondaryStage.setTitle("BLOODLINK — FrontOffice");
+        secondaryStage.setScene(scene2);
+        secondaryStage.show();
     }
 
     public static void main(String[] args) {
